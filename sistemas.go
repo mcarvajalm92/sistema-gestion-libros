15e79e56e8a9862c310da8f07302c07491928427
Mario Carvajal <mcarvajalm92@gmail.com>
2026-07-11 12:39:47 -0500

    Menú principal del sistema de gestión de libros

--- sistemas.go
+++ sistemas.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hola, sistema de libros")
+	lector := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("\n1. Agregar")
+		fmt.Println("2. Listar")
+		fmt.Println("3. Buscar")
+		fmt.Println("4. Cambiar Estado")
+		fmt.Println("5. Eliminar")
+		fmt.Println("6. Salir")
+		fmt.Print("Opción: ")
+
+		opcion, _ := lector.ReadString('\n')
+		opcion = string(opcion[0])
+
+		switch opcion {
+		case "1":
+			fmt.Println("Función Agregar - en construcción")
+		case "2":
+			fmt.Println("Función Listar - en construcción")
+		case "3":
+			fmt.Println("Función Buscar - en construcción")
+		case "4":
+			fmt.Println("Función Cambiar Estado - en construcción")
+		case "5":
+			fmt.Println("Función Eliminar - en construcción")
+		case "6":
+			fmt.Println("Adiós")
+			return
+		default:
+			fmt.Println("Opción no válida")
+		}
+	}
 }
